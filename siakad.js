91aaa17dcd6a3e75af182981754c5faf687c398a
Gagassurya19 <gagassuryalaksana04@gmail.com>
2020-09-16 15:26:14 +0700

    headless: true

--- siakad.js
+++ siakad.js
@@ -10,6 +10,7 @@ const siakad = {
 
     initialize: async () => {
         siakad.browser = await puppeteer.launch({
+            headless: true,
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
 
